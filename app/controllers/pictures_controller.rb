8a070fb2a4b695ca67b6aa0a07551bc108a5f6b8
llleelay <llleelay@gmail.com>
2015-04-02 00:54:19 +0800

    show picture page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,11 @@
 class PicturesController < ApplicationController
+
+  def show
+    @picture = Picture.find_by!(id: params[:id], active: true)
+    @catalog = @picture.catalog
+    @job = @catalog.person.job
+    @person = @catalog.person
+    @people = Person.where(job_id: @job.id, active: true)
+  end
+
 end
